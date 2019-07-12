2e9b9f96d79a22aaf009509620d9fc0c217c231b
AndrewHanson1 <47340852+AndrewHanson1@users.noreply.github.com>
2019-07-11 20:55:14 -0700

    basic routes and controllers

---
+++ controllers/adminController.js
@@ -0,0 +1,44 @@
+const db = require("../models");
+
+// Defining methods for the adminController
+module.exports = {
+  findAll: function(req, res) {
+    db.Customer
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Customer
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  createUser: function(req, res) {
+    db.Customer
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findAllRequests: function(req, res) {
+    db.Asset
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findOneRequest: function(req, res) {
+    db.Asset
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Customer
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
---
+++ routes/api/customerApi.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const customerController = require("../../controllers/customerController");
+
+
+router.route("/dashboard/schedule")
+  .post(customerController.createSchedule);
+
+router.route("/dashboard/requests:id")
+  .get(customerController.findAllRequests);
+
+router.route("/dashboard/bills")
+  .get(customerController.findBills);
+
+module.exports = router;
+
