f7bec7f21c5607b54e1a9a6dede0afb2e4475b9c
AndrewHanson1 <47340852+AndrewHanson1@users.noreply.github.com>
2019-07-14 10:14:21 -0700

    new user route

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,13 +1,16 @@
 const db = require("../models");
+const axios = require("axios");
+
 
 // Defining methods for the adminController
 module.exports = {
   findAll: function(req, res) {
-    db.Customer
-      .find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+    // db.Customer
+    //   .find(req.query)
+    //   .sort({ date: -1 })
+    //   .then(dbModel => res.json(dbModel))
+    //   .catch(err => res.status(422).json(err));
+      res.send('Here you Go');
   },
   findById: function(req, res) {
     db.Customer
@@ -20,6 +23,8 @@ module.exports = {
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+
+      
   },
   findAllRequests: function(req, res) {
     db.Asset
@@ -40,5 +45,40 @@ module.exports = {
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+
+  addNewUser: function(req, res){
+
+    const userObj = req.body;
+
+    console.log("adding new user to database: ");
+    console.log("-------------------------------");
+    
+  
+    console.log(req.body.client_id);
+
+
+    
+
+
+    axios
+    .post('https://dev-23nqtwhs.auth0.com/dbconnections/signup', 
+    { 
+      client_id : req.body.client_id,
+      email : req.body.email,
+      password : req.body.password,
+      connection : req.body.connection,
+      name : req.body.name,
+      user_metadata : req.body.user_metadata
+
+     })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    
+   res.status(200).send(userObj);
   }
 };
--- routes/api/adminApi.js
+++ routes/api/adminApi.js
@@ -2,23 +2,27 @@ const router = require("express").Router();
 const adminController = require("../../controllers/adminController");
 
 
-router.route("/admin/customers")
-  .get(adminController.findAllUser);
 
-  router.route("/admin/customers/:id")
-  .get(adminController.findOneUser);
+router.route("/customers")
+  .get(adminController.findAll);
 
-  router.route("/admin/customers/add")
-  .post(adminController.createUser);
+  router.route("/newUser")
+  .post(adminController.addNewUser);
 
-  router.route("/admin/requests")
-  .get(adminController.findAllRequests);
+//   router.route("/customers/:id")
+//   .get(adminController.findOneUser);
 
-  router.route("/admin/requests/:id")
-  .get(adminController.findOneRequest)
+//   router.route("/customers/add")
+//   .post(adminController.createUser);
 
-  router.route("/admin/calendar")
-  .get(adminController.calendar);
+//   router.route("/requests")
+//   .get(adminController.findAllRequests);
+
+//   router.route("/requests/:id")
+//   .get(adminController.findOneRequest)
+
+//   router.route("/calendar")
+//   .get(adminController.calendar);
 
 
 
