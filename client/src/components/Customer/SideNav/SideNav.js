12e3b4e38836fdca10a0f85b364113c80001338f
bryceacampbell <bryceacampbell7@gmail.com>
2019-07-17 23:41:47 -0700

    adding font awesome icons to side nav

--- client/src/components/Customer/SideNav/SideNav.js
+++ client/src/components/Customer/SideNav/SideNav.js
@@ -1,6 +1,8 @@
 import React from "react";
-
 import "./style.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faWrench, faDollarSign } from "@fortawesome/free-solid-svg-icons";
 
 const SideNav = props => {
 
@@ -13,10 +15,10 @@ const SideNav = props => {
         <nav className={sideNavClasses} >
             <img className="logo" src="http://www.dev202valet.com.php72-35.phx1-1.websitetestlink.com/assets/images/svg/202_valet_white.svg" alt="202 valet logo"></img>
             <ul>
-                <li><a href="/dashboard/home">Home</a></li>
-                <li><a href="/dashboard/schedule">Schedule A Service</a></li>
-                <li><a href="/dashboard/requests">Current Requests</a></li>
-                <li><a href="/dashboard/bills">View Bills</a></li>
+                <li><FontAwesomeIcon icon={faHome} className="nav-icons"/><a href="/dashboard/home">Home</a></li>
+                <li><FontAwesomeIcon icon={faWrench} className="nav-icons"/><a href="/dashboard/schedule">Schedule A Service</a></li>
+                <li><FontAwesomeIcon icon={faWrench} className="nav-icons"/><a href="/dashboard/requests">Current Requests</a></li>
+                <li><FontAwesomeIcon icon={faDollarSign} className="nav-icons"/><a href="/dashboard/bills">View Bills</a></li>
             </ul>
         </nav>
     )
--- client/src/components/admin/SideNav/SideNav.js
+++ client/src/components/admin/SideNav/SideNav.js
@@ -1,6 +1,8 @@
 import React from "react";
-
 import "./style.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faWrench, faUsers, faCalendarPlus } from "@fortawesome/free-solid-svg-icons";
 
 const SideNav = props => {
 
@@ -13,10 +15,10 @@ const SideNav = props => {
         <nav className={sideNavClasses} >
             <img className="logo" src="http://www.dev202valet.com.php72-35.phx1-1.websitetestlink.com/assets/images/svg/202_valet_white.svg" alt="202 valet logo"></img>
             <ul>
-                <li><a href="/admin">Home</a></li>
-                <li><a href="/admin/requests">View Requests</a></li>
-                <li><a href="/admin/customers">Customers</a></li>
-                <li><a href="/admin/calendar">Calendar</a></li>
+                <li><FontAwesomeIcon icon={faHome} className="nav-icons"/><a href="/admin"> Home</a></li>
+                <li><FontAwesomeIcon icon={faWrench} className="nav-icons"/><a href="/admin/requests"> View Requests</a></li>
+                <li><FontAwesomeIcon icon={faUsers} className="nav-icons"/><a href="/admin/customers"> Customers</a></li>
+                <li><FontAwesomeIcon icon={faCalendarPlus} className="nav-icons"/><a href="/admin/calendar"> Calendar</a></li>
             </ul>
         </nav>
     )
