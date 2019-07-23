62ee299aae86e6e4865529402f224bfcd6249fbc
bryceacampbell <bryceacampbell7@gmail.com>
2019-07-22 21:32:00 -0700

    adding calendar

--- client/src/auth/auth0-variables.js
+++ client/src/auth/auth0-variables.js
@@ -2,6 +2,6 @@ export const AUTH_CONFIG = {
     domain: "dev-23nqtwhs.auth0.com",
     roleUrl: "https://dry-waters-95011.herokuapp.com/role",
     clientId: "b3MTC80Frslwa1jungIpq65noxzGJ22B",
-    callbackUrl: "https://dry-waters-95011.herokuapp.com/callback"
+    callbackUrl: "http://localhost:3000/callback"
   };
   
---
+++ client/src/components/admin/Calendar/Calendar.js
@@ -0,0 +1,129 @@
+import React from "react";
+import "./style.css";
+
+import format from "date-fns/format";
+import addMonths from "date-fns/add_months";
+import startOfWeek from "date-fns/start_of_week";
+import endOfWeek from "date-fns/end_of_week";
+import addDays from "date-fns/add_days";
+import startOfMonth from "date-fns/start_of_month";
+import endOfMonth from "date-fns/end_of_month";
+import isSameMonth from "date-fns/is_same_month";
+import isSameDay from "date-fns/is_same_day";
+import parse from "date-fns/parse";
+
+class Calendar extends React.Component {
+    state = {
+        currentMonth: new Date(),
+        selectedDate: new Date()
+    };
+
+    renderHeader() {
+        const dateFormat = "MMMM YYYY";
+        return (
+            <div className="header row flex-middle">
+                <div className="col col-start">
+                    <div className="icon" onClick={this.prevMonth}>
+                        chevron_left
+                    </div>
+                </div>
+                <div className="col col-center">
+                    <span>
+                        {format(this.state.currentMonth, dateFormat)}
+                    </span>
+                </div>
+                <div className="col col-end" onClick={this.nextMonth}>
+                    <div className="icon">chevron_right</div>
+                </div>
+            </div>
+        );
+    };
+
+    renderDays() {
+        const dateFormat = "dddd";
+        const days = [];
+        let startDate = startOfWeek(this.state.currentMonth);
+        for (let i = 0; i < 7; i++) {
+            days.push(
+                <div className="col col-center" key={i}>
+                    {format(addDays(startDate, i), dateFormat)}
+                </div>
+            );
+        }
+        return <div className="days row">{days}</div>;
+    };
+
+    renderCells() {
+        const { currentMonth, selectedDate } = this.state;
+        const monthStart = startOfMonth(currentMonth);
+        const monthEnd = endOfMonth(monthStart);
+        const startDate = startOfWeek(monthStart);
+        const endDate = endOfWeek(monthEnd);
+
+        const dateFormat = "D";
+        const rows = [];
+        let days = [];
+        let day = startDate;
+        let formattedDate = "";
+
+        while (day <= endDate) {
+            for (let i = 0; i < 7; i++) {
+                formattedDate = format(day, dateFormat);
+                const cloneDay = day;
+                days.push(
+                    <div
+                        className={`col cell ${
+                            !isSameMonth(day, monthStart)
+                                ? "disabled"
+                                : isSameDay(day, selectedDate) ? "selected" : ""
+                            }`}
+                        key={day}
+                        onClick={() => this.onDateClick(parse(cloneDay))}
+                    >
+                        <span className="number">{formattedDate}</span>
+                        <span className="bg">{formattedDate}</span>
+                    </div>
+                );
+                day = addDays(day, 1);
+            }
+            rows.push(
+                <div className="row" key={day}>
+                    {days}
+                </div>
+            );
+            days = [];
+        };
+        return <div className="body">{rows}</div>;
+    }
+
+    onDateClick = day => {
+        this.setState({
+            selectedDate: day
+        });
+    };
+
+    nextMonth = () => {
+        this.setState({
+            currentMonth: addMonths(this.state.currentMonth, 1)
+        });
+    };
+
+    prevMonth = () => {
+        this.setState({
+            currentMonth: addMonths(this.state.currentMonth, -1)
+        });
+    };
+
+    render() {
+
+        return (
+            <div className="calendar">
+                {this.renderHeader()}
+                {this.renderDays()}
+                {this.renderCells()}
+            </div>
+        );
+    }
+};
+
+export default Calendar;
