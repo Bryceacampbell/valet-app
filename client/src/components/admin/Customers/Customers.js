2ccf0112d6227ae54661e48562d9e55eed75d437
AndrewHanson1 <47340852+AndrewHanson1@users.noreply.github.com>
2019-07-25 21:20:42 -0700

    added promise to create mongo user and auth0 user with an identical ID

--- client/src/components/admin/Customers/Customers.js
+++ client/src/components/admin/Customers/Customers.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import API from "../../../utils/API";
 import "./style.css";
+import CustomerModal from "../CustomersModal/CustomersModal.js";
+import CustomerBackdrop from "./backdrop";
 
 import { Table } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,6 +13,7 @@ class Customers extends Component {
 
     state = {
         customers: [],
+        modalSwitch: false,
     };
 
     componentDidMount() {
@@ -26,31 +29,61 @@ class Customers extends Component {
             .catch(err => console.log(err.response));
     };
 
+    customerClicked = () => {
+        // event.persist();
+        this.setState({
+            modalSwitch: true
+
+        });
+
+
+
+        console.log("state from clicking request");
+        console.log(this.state);
+
+    }
+
     render() {
         return (
+
             <div className="container-fluid card text-center">
+
+
+
+
+
+                {this.state.modalSwitch && <CustomerBackdrop />}
+                {this.state.modalSwitch && <CustomerModal>
+                    <p>Modal Content</p>
+                </CustomerModal>}
+
+
+
                 <div className="card-header row">
-                <div className="col-lg-1">
+                    <div className="col-lg-1">
                     </div>
                     <div className="col-lg-10">
                         <h3>Customers</h3>
                     </div>
                     <div className="col-lg-1">
-                        <a href="/admin/newuser"><FontAwesomeIcon icon={faPlusCircle} className="add-btn"/></a>
+                        <a href="/admin/newuser"><FontAwesomeIcon icon={faPlusCircle} className="add-btn" /></a>
                     </div>
                 </div>
                 <div className="card-body">
+
+
+
                     <div className="row">
                         <div className="col-lg-12 search-bar">
                             <div className="form-group has-search">
-                                <span className="form-control-feedback"><FontAwesomeIcon icon={faSearch}/></span>
+                                <span className="form-control-feedback"><FontAwesomeIcon icon={faSearch} /></span>
                                 <input type="text" className="form-control" placeholder="Search"></input>
                             </div>
                         </div>
                     </div>
                     <Table striped bordered hover>
                         <thead>
-                            <tr>
+                            <tr >
 
                                 <th>First Name</th>
                                 <th>Last Name</th>
@@ -60,7 +93,7 @@ class Customers extends Component {
                         </thead>
                         <tbody>
                             {this.state.customers.map(customer => (
-                                <tr key="customer._id">
+                                <tr onClick={() => this.customerClicked()} key="customer._id">
                                     <td>{customer.information.firstName}</td>
                                     <td>{customer.information.lastName}</td>
                                     <td>{customer.information.phoneNumber}</td>
@@ -71,6 +104,7 @@ class Customers extends Component {
                     </Table>
                 </div>
             </div>
+
 
         )
     }
---
+++ client/src/components/admin/Customers/backdrop.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./style.css"
+
+const CustomerBackdrop = props => <div className="customerbackdrop"></div>;
+
+export default CustomerBackdrop;
