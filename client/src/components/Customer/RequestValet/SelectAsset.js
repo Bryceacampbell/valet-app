fab7594397b7d60958a0b1d197bd5991c3da9cc6
bryceacampbell <bryceacampbell7@gmail.com>
2019-08-13 11:59:40 -0700

    adding select state for choosing an asset

--- client/src/components/Customer/RequestValet/SelectAsset.js
+++ client/src/components/Customer/RequestValet/SelectAsset.js
@@ -2,10 +2,25 @@ import React, { Component } from "react";
 
 import API from "../../../utils/API";
 
+var styles = {
+
+    selected: {
+        border: "2px solid green",
+        "border-radius": ".5rem" 
+    },
+
+    notSelected: {
+        border: "transparent"
+    }
+
+
+};
+
 class SelectAsset extends Component {
 
     state = {
-        assets: []
+        assets: [],
+
     };
 
     componentDidMount() {
@@ -23,39 +38,59 @@ class SelectAsset extends Component {
             .catch(err => console.log(err.response));
     };
 
+    handleSelect = ({ name, value }) => {
+        // console.log(name, value);
+        this.props.handleChange({ name, value });
+
+        this.setState({
+            [value]: !this.state[value] ? true : false
+        });
+
+    };
+
+
+
     render() {
         if (this.props.currentStep !== 1) {
             return null
         }
         return (
             <div>
-                <div className="text-center">Select Asset</div>
+                <div className="text-center"><strong>Step 1:</strong> Select An Asset</div>
                 <hr></hr>
-                {this.state.assets.map(asset => (
-                    <div className="card"
-                        key={(asset._id)}
-                        id={(asset._id)}
-                        onClick={() => this.props.handleChange({
-                            name: "assetId",
-                            value: asset._id
-                        })}
-                    >
-                        <div className="row">
-                            <div className="col-lg-4">
-                                <p>Year:</p>
-                                <p>{asset.description.year}</p>
-                            </div>
-                            <div className="col-lg-4">
-                                <p>Make:</p>
-                                <p>{asset.description.make}</p>
-                            </div>
-                            <div className="col-lg-4">
-                                <p>Model:</p>
-                                <p>{asset.description.model}</p>
+
+
+                {this.state.assets.map(asset => {
+                    const selected = this.state[asset._id];
+                    return (
+                        <div
+                            key={(asset._id)}
+                            style={selected ? styles.selected : styles.notSelected}>
+                            <div className="card"
+                                id={(asset._id)}
+                                onClick={() => this.handleSelect({
+                                    name: "assetId",
+                                    value: asset._id
+                                })}
+                            >
+                                <div className="row">
+                                    <div className="col-lg-4">
+                                        <p>Year:</p>
+                                        <p>{asset.description.year}</p>
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <p>Make:</p>
+                                        <p>{asset.description.make}</p>
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <p>Model:</p>
+                                        <p>{asset.description.model}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    )
+                })}
             </div>
         )
     }
