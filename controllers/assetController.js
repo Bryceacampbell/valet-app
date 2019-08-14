36a6243965e20d97b59b3f5129457faabe63a8da
bryceacampbell <bryceacampbell7@gmail.com>
2019-08-14 16:42:01 -0700

    removing line

--- controllers/assetController.js
+++ controllers/assetController.js
@@ -51,7 +51,6 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
 
-      res.redirect("/dashboard"); //this isnt working
   },
 
   createAsset: function (req, res) {
